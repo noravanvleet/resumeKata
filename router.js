66527c5c37cdb8ce8acad3f8bd7381a30e0a3ca1
Nora Van Vleet <noravanvleet@gmail.com>
2021-08-02 09:11:52 -0400

    created a router file

---
+++ router.js
@@ -0,0 +1,13 @@
+const { Router } = require('express')
+
+async function buildRouter() {
+    let router = new Router()
+
+    router.get('/', (req, res) => res.sendFile(`${__dirname}index.html`))
+
+    return router
+}
+
+module.exports = {
+    buildRouter: buildRouter
+}
