87880064c33f5f3a44d63103a0ce870a8f38b4b0
Nora Van Vleet <noravanvleet@gmail.com>
2021-08-02 17:13:00 -0400

    set up local host with basic html

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const { Router } = require('express')
 async function buildRouter() {
     let router = new Router()
 
-    router.get('/', (req, res) => res.sendFile(`${__dirname}index.html`))
+    router.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
 
     return router
 }
