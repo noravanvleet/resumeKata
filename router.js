56e37bec25292f5def8133c2dd6feedf377c7797
Nora Van Vleet <noravanvleet@gmail.com>
2021-08-09 10:13:14 -0400

    setup parts of testing and the api

--- router.js
+++ router.js
@@ -5,8 +5,20 @@ async function buildRouter() {
 
     router.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
 
+    router.get('/seven', (req, res) => {
+
+    })
+
+    router.get('/seven', (req, res) => {
+        let request = req.body
+        let answer = mathKata(request)
+        //Take the data send and run it through the Kata
+        //Send back the solution
+        res.json({ answer: answer })
+    })
     return router
 }
+
 
 module.exports = {
     buildRouter: buildRouter
