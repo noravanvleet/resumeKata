190b2c0fdca752049c0fc28459de8759c52598da
Nora Van Vleet <noravanvleet@gmail.com>
2021-08-09 15:52:29 -0400

    fixed post request

--- public/js/backendRequest.js
+++ public/js/backendRequest.js
@@ -2,15 +2,14 @@ function handleSevenKata(){
     event.preventDefault();
     console.log("reached")
  let input = document.getElementById("numberField").value
-    axios.get('https://resumekatas.herokuapp.com/divideBySeven', { params: { input: input } })
+    let request = { input: input }
+    axios.post('https://resumekatas.herokuapp.com/divideBySeven', request)
         .then(function (response) {
             let output = document.getElementById('outputForSeven')
-            output.innerHTML = response.answer
+            output.innerHTML = response.data.result
         })
         .catch(function (error) {
             console.log(error)
         });
 }
-
-
 
--- router.js
+++ router.js
@@ -1,17 +1,19 @@
 const { Router } = require('express')
 
-const {seven} = require("./public/js/divideBy7Kata")
+const { seven } = require("./public/js/divideBy7Kata")
 
 async function buildRouter() {
     let router = new Router()
 
     router.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
 
-    router.get('/divideBySeven', (req, res) => {
-        let request = req.query.input
-        let answer = seven(request)
-        res = res.json({ answer: answer })
-     })
+    router.post('/divideBySeven', (req, res) => {
+        let input = req.body.input
+        if(input) {
+            let result = seven(input)
+            res.status(200).json({"result" : result})
+        }
+    })
 
     return router
 }
