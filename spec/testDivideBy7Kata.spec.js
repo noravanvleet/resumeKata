6204644752963b913827ec8e04df9c23bf3af630
Nora Van Vleet <noravanvleet@gmail.com>
2021-08-11 12:06:30 -0400

    implemented jest in order to get testing to run

--- spec/testDivideBy7Kata.spec.js
+++ spec/testDivideBy7Kata.spec.js
@@ -1,5 +1,5 @@
 const { seven } = require("../public/js/divideBy7Kata")
-const jest = require("jest")
+//const jest = require("jest")
 const fs = require('fs')
 let path = require('path')
 const testDataFile = path.resolve(__dirname, './data/testData.json')
@@ -9,8 +9,12 @@ let projectObject = JSON.parse(testData)
 describe ("division by 7", () => {
     it ("should return an array with the number left after passes and the number of passes", () =>{
         let result = seven(378)
-        expect(result[0]).toEqual(36)
+        expect(result[0]).toEqual(21)
         expect(result[1]).toEqual(1)
+    })
+    it ("should return an array", () =>{
+        let result = seven(371)
+        expect(result).toEqual([35,1])
     })
 })
 
