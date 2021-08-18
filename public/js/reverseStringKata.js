d96ce77eb46e02ea5849307552461e4b896f357a
Nora Van Vleet <noravanvleet@gmail.com>
2021-08-18 10:05:03 -0500

    fixed overwriting variable issue and added additional kata testing

--- public/js/reverseStringKata.js
+++ public/js/reverseStringKata.js
@@ -1,7 +1,31 @@
+
+// Complete the function that accepts a string parameter, and reverses each word in the string.
+// All spaces in the string should be retained.
+
 const axios = require("axios")
 
-function reverse() {
+function reverse(stringToReverse) {
+    let reversedWord = ""
+    let returnString = ""
+    let returnArray = stringToReverse.split(" ")
+    for(let i = 0; i < returnArray.length; i++){
+        reversedWord = reverseWord(returnArray[i])
+        if(i == 0){
+            returnString = reversedWord
+        } else{
+            returnString = `${returnString} ${reversedWord}`
+        }
+    }
+    return returnString
+}
 
+function reverseWord(word) {
+    let returnStr = ""
+    let tailIndex = word.length -1
+    for(let i = tailIndex; i > -1; i--){
+        returnStr = returnStr.concat(word[i])
+    }
+    return returnStr
 }
 
 const reverseString = (e) => {
@@ -18,5 +42,7 @@ const reverseString = (e) => {
 
 }
 module.exports = {
-    reverse: reverse
+    reverse: reverse,
+    reverseWord: reverseWord
+
 }
